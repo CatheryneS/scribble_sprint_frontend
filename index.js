3d20c45a592312a92d72c8e0ea3b6b35c5061edf
Catheryn Small <catheryn.small@gmail.com>
2020-12-13 20:48:09 -0500

    add URLs for fetch usage

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log("we're connected!")
+const BASEURL = "http://localhost:3000/api/v1/"
+const NOTEBOOKURL = BASEURL + "notebooks"
+const PROMPTURL = BASEURL + "prompts"
+const STORYURL = BASEURL + "stories"
+
+fetch(PROMPTURL)
+.then(resp => resp.json())
+.then(obj => {
+    console.log(obj)
+})
