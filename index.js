a8222bdbb186b79a20370fb560f59ec445e1a469
Catheryn Small <catheryn.small@gmail.com>
2020-12-13 22:55:03 -0500

    create notebook cards

--- index.js
+++ index.js
@@ -3,8 +3,25 @@ const NOTEBOOKURL = BASEURL + "notebooks"
 const PROMPTURL = BASEURL + "prompts"
 const STORYURL = BASEURL + "stories"
 
-fetch(PROMPTURL)
-.then(resp => resp.json())
-.then(obj => {
-    console.log(obj)
-})
+function getNotebooks(){
+    fetch(NOTEBOOKURL)
+    .then(resp => resp.json())
+    .then(notebooks => notebooks.data.forEach(createNotebook))
+}
+getNotebooks()
+
+function createNotebook(notebook){
+    const row = document.getElementById('notebooks');
+    const column = document.createElement('section');
+    const card = document.createElement('section');
+    const h4Tag = document.createElement('h4');
+
+    column.className = 'column';
+    card.className = 'card';
+    h4Tag.id = `${notebook.attributes.name}`;
+    h4Tag.innerText = `${notebook.attributes.name}`;
+
+    card.appendChild(h4Tag);
+    row.appendChild(column);
+    column.appendChild(card);
+}
