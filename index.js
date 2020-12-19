0a1be87ca1b7cfe46988dce685b1a625c3a928b1
Catheryn Small <catheryn.small@gmail.com>
2020-12-19 14:10:36 -0500

    add view all and select another notebook functions

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ function openNotebook(selection) {
     story.style.display = "block";
 
     showTimer();
-    grabPrompt(selection.target.parentElement.id);
+    grabPrompt(selection.target.parentElement.dataset.id);
 }
 
 function grabPrompt(notebookSelected) {
@@ -147,25 +147,53 @@ function renderStoryPage(storyObj) {
     const form = document.getElementById('story-form');
     const h1Tag = document.createElement('h1');
     const pTag = document.createElement('p');
+    const btn1 = document.createElement('button');
+    const btn2 = document.createElement('button');
 
     if (prompt && timer && form) {
         prompt.remove();
         timer.remove();
         form.remove();
     } else {
-        document.getElementById('notebook-index').remove();
+        document.getElementById('notebook-index').style.display = "none";
+        let title = document.getElementsByClassName('Titles');
+
+        while(title[0]) {
+            title[0].parentNode.removeChild(title[0]);
+        }
         storySect.style.display = "block";
     }
 
     h1Tag.className = "story-title";
     pTag.className = "story-content";
+    btn1.dataset.id = `${storyObj.data.attributes.prompt.notebook_id}`;
+    btn1.innerText = "View all stories";
+    btn1.addEventListener('click', e => {
+        removeStory();
+        getStories(storyObj.data.attributes.prompt.notebook_id);
+    });
+    btn2.innerText = "Pick another notebook";
+    btn2.addEventListener('click', e => {
+        removeStory();
+        showNotebooks();
+    });
+
 
     h1Tag.innerText = storyObj.data.attributes.title;
     pTag.innerText = storyObj.data.attributes.content;
 
     storySect.appendChild(h1Tag);
     storySect.appendChild(pTag);
-    
+    storySect.appendChild(btn1);
+    storySect.appendChild(btn2);
+}
+
+function removeStory() {
+    document.getElementById('story').style.display = "none";
+    document.getElementsByClassName('story-title')[0].remove();
+    document.getElementsByClassName('story-content')[0].remove();
+    document.querySelector("#story > button:nth-child(1)").remove();
+    document.querySelector("#story > button").remove();
 }
 
 function readNotebook(notebook) {
@@ -182,6 +210,7 @@ function getStories(notebookId){
     fetch(NOTEBOOKURL + "/" + notebookId)
     .then(resp => resp.json())
     .then(notebook => notebook.data.attributes.stories.forEach(createIndex))
+    .catch(error => {return "No stories have been written"})
 }
 
 function createIndex(story) {
@@ -190,6 +219,7 @@ function createIndex(story) {
     const liTag = document.createElement('li');
     const btn = document.createElement('button');
 
+    index.style.display = "block";
     ulTag.className = "Titles";
     liTag.className = "Title";
     liTag.innerText = `${story.title}`;
@@ -204,7 +234,18 @@ function createIndex(story) {
 
 function getStory(story){
     const id = story.target.dataset.id;
+
     fetch(STORYURL + "/" + id)
     .then(resp => resp.json())
     .then(obj => renderStoryPage(obj))
+}
+
+function showNotebooks() {
+    const intro = document.getElementById('intro');
+    const row = document.getElementById('notebooks');
+    const story = document.getElementById('story');
+
+    intro.style.display = "block";
+    row.style.display = "block";
+    story.style.display = "none";
 }
