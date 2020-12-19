0152caabfda816ab8d50aa206a0a70609dd4d356
Catheryn Small <catheryn.small@gmail.com>
2020-12-18 19:05:56 -0500

    save story to database and render to page

--- index.js
+++ index.js
@@ -71,11 +71,14 @@ function createStoryForm(prompt) {
     const title = document.createElement('input');
     const btn = document.createElement('button');
 
-    form.id = `${prompt.data.id}`;
-    label.innerText = "Title";
+    form.dataset.id = `${prompt.data.id}`;
+    form.id = "story-form";
     title.id = "Title";
+    title.type = "text";
+    text.id = "story-area";
     btn.innerText = "Save";
-    btn.addEventListener('submit', saveStory);
+    btn.type = "submit";
+    form.addEventListener('submit', saveStory);
 
     section.appendChild(form);
     form.appendChild(label);
@@ -99,21 +102,59 @@ let time = start * 60;
 
 function updateTimer() {
     const timer = document.getElementById('timer');
-    const mins = Math.floor(time/60);
-    let secs = time % 60;
+    if (timer){
+        const mins = Math.floor(time/60);
+        let secs = time % 60;
 
-    secs = secs < 10 ? '0' + secs : secs;
+        secs = secs < 10 ? '0' + secs : secs;
 
-    timer.innerHTML = `${mins}:${secs}`;
-    time--;
+        timer.innerHTML = `${mins}:${secs}`;
+        time--;
 
-    if (secs == 0 && mins == 0) {
-        clearInterval(startTimer);
-        timer.innerHTML = "Time's Up!"
+        if (secs == 0 && mins == 0) {
+            clearInterval(startTimer);
+            timer.innerHTML = "Time's Up!"
+        }
     }
 }
 
-function saveStory(){
-    debugger
+function saveStory(e){
+    e.preventDefault();
+    fetch(STORYURL,{
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify({
+            title: e.target.children[1].value,
+            content: e.target.children[2].value,
+            prompt_id: e.target.dataset.id
+        })
+    })
+    .then(resp => resp.json())
+    .then(obj => renderStory(obj));
+}
 
+function renderStory(storyObj) {
+    const prompt = document.getElementById('prompt');
+    const storySect = document.getElementById('story');
+    const timer = document.getElementById('timer');
+    const form = document.getElementById('story-form');
+    const h1Tag = document.createElement('h1');
+    const pTag = document.createElement('p');
+
+    prompt.remove();
+    timer.remove();
+    form.remove();
+
+    h1Tag.className = "story-title";
+    pTag.className = "story-content";
+
+    h1Tag.innerText = storyObj.data.attributes.title;
+    pTag.innerText = storyObj.data.attributes.content;
+
+    storySect.appendChild(h1Tag);
+    storySect.appendChild(pTag);
+    
 }
